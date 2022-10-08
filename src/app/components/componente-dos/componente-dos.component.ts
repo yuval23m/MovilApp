008411f5ac71d3cfb24819354eea7857dbc1bd3c
yuval23m <y.uval@duocuc.cl>
2022-10-07 23:43:16 -0300

    CRUD AGREGAR VIAJE LISTO EN VISTA

--- src/app/components/componente-dos/componente-dos.component.ts
+++ src/app/components/componente-dos/componente-dos.component.ts
@@ -2,6 +2,8 @@ import { Viajes } from './../../interfaces/viajes';
 import { FirestoreService } from './../../services/firestore.service';
 import { Component, OnInit } from '@angular/core';
 
+import { generateSlug } from "random-word-slugs";
+
 @Component({
   selector: 'app-componente-dos',
   templateUrl: './componente-dos.component.html',
@@ -19,11 +21,14 @@ export class ComponenteDosComponent implements OnInit {
 
   ngOnInit() {}
 
+  
 
   crearNuevoViaje(){
+    const slug = generateSlug(1, {format: "title"});
     const path = 'Viajes'
-    this.firestore.crearViaje(this.viaje, path, 'SIFUNCIONA').then((res)=>{
-      console.log('Se agrego correctamente', res);
+    this.firestore.crearViaje(this.viaje, path, slug).then((res)=>{
+      console.log('Se agrego correctamente a la base de datos');
     });
   }
+
 }
