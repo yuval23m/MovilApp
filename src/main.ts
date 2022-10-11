6dc233070c5e68e01f8e974e9aa69f500ba5b0f8
yuval23m <y.uval@duocuc.cl>
2022-10-11 01:40:00 -0300

    eliminado background video

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-//BACKGROUND
-import {defineCustomElements} from 'gl-ionic-background-video/dist/loader';
 
 if (environment.production) {
   enableProdMode();
@@ -12,4 +10,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
-defineCustomElements(window)//background
