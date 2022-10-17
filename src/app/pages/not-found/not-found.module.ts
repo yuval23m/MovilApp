79075d6efdd26e8ee5be11418cf64103197fd7c2
yuval23m <y.uval@duocuc.cl>
2022-10-17 01:27:07 -0300

    animacion mejor

--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,14 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { NotFoundPageRoutingModule } from './not-found-routing.module';
 
 import { NotFoundPage } from './not-found.page';
+//lottie
+import {LottieModule} from 'ngx-lottie'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    NotFoundPageRoutingModule
+    NotFoundPageRoutingModule,
+    LottieModule
   ],
-  declarations: [NotFoundPage]
+  declarations: [NotFoundPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NotFoundPageModule {}
--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
+import { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'app-not-found',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundPage implements OnInit {
 
+    //lottie
+    options: AnimationOptions = {
+      path: "/assets/notf.json"
+    }
+
   constructor() { }
+
+      //lottie
+      animationCreada(animationItem: AnimationItem):void{
+        console.log('animacion cargada exitosamente');
+      }
 
   ngOnInit() {
   }
